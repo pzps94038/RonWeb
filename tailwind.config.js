7a8f07d4bbda4361ca0b0db83ed85223ea781361
ron.ro <pzps94038@yahoo.com.tw>
2023-04-10 20:53:20 +0800

    fix: 更新tailwind設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.html", "./src/**/*.scss"],
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
-
+};
