f1975d1f75f5527dbec6b5f46016ec77b385fcae
ron.ro <pzps94038@yahoo.com.tw>
2023-04-10 20:56:19 +0800

    fix: 調整tailwind設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.html", "./src/**/*.scss"],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.html", "./src/**/*.scss"],
   theme: {
     extend: {},
   },
