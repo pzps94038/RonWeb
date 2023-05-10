7047495898a5d10854cf6a355762123be57f59cb
ron.ro <pzps94038@yahoo.com.tw>
2023-05-10 11:40:28 +0800

    fix: 調整響應式

--- src/app/blog/category/category.component.ts
+++ src/app/blog/category/category.component.ts
@@ -77,6 +77,8 @@ export class CategoryComponent implements OnInit {
         takeUntilDestroyed(this._destroyRef),
       )
       .subscribe(res => {
+        console.warn(res);
+        console.warn(res.returnCode, res.returnCode === ReturnCode.NotFound);
         if (this.sharedSrv.ifSuccess(res)) {
           const {
             data: { total, articles, keyword },
