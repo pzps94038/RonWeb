64cb84307bf08309335df5f9eadc6e801b770ba9
ron.ro <pzps94038@yahoo.com.tw>
2023-05-13 22:08:55 +0800

    fix: 新增editor

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { EditorComponent } from './shared/components/editor/editor.component';
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent, EditorComponent],
   templateUrl: './setting.component.html',
   styleUrls: ['./setting.component.scss'],
 })
