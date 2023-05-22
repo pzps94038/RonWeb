d2df378342d5f6d8d46e953e25021f6ade69bfe7
ron.ro <pzps94038@yahoo.com.tw>
2023-05-22 20:44:59 +0800

    fix: 調整自動產生siteMap

--- sitemap.js
+++ sitemap.js
@@ -3,80 +3,108 @@ const { resolve } = require('path');
 const axios = require('axios');
 const { createGzip } = require('zlib');
 const { SitemapAndIndexStream, SitemapStream } = require('sitemap');
-(async () => {
-  const { data } = await axios('https://ronweb-api.zeabur.app/api/siteMap/article').catch(error => {
-    // 處理錯誤
-    console.error(error);
-  });
-  const { returnCode } = data;
-  if (returnCode === '00') {
-    const { data: list } = data;
-    console.warn(JSON.stringify(list));
-  }
-})();
 
-const urls = [
-  { url: '/page-1/', changefreq: 'daily' },
-  { url: '/page-2/', changefreq: 'weekly' },
-  { url: '/page-3/', changefreq: 'monthly' },
-  { url: '/page-4/', changefreq: 'weekly' },
-  { url: '/page-5/', changefreq: 'monthly' },
-  { url: '/page-6/', changefreq: 'daily' },
-  { url: '/page-7/', changefreq: 'weekly' },
-  { url: '/page-8/', changefreq: 'monthly' },
-  { url: '/page-9/', changefreq: 'daily' },
-  { url: '/page-10/', changefreq: 'weekly' },
+const lastmod = new Date();
+
+let urls = [
+  { url: '/login', changefreq: 'monthly', priority: 0.8, lastmod },
+  { url: '/about-me', changefreq: 'weekly', priority: 1.0, lastmod },
+  { url: '/blog', changefreq: 'daily', priority: 1.0, lastmod },
+  { url: '/notFound', changefreq: 'monthly', priority: 0.5, lastmod },
 ];
 
-// 每幾個網址分割成一個 sitemap 檔案
-const urlsPerSitemap = 20000;
-// 計算 sitemap 檔案數量
-let sitemapCount = 0;
+(async () => {
+  const [articles, categorys, labels] = await Promise.all([
+    axios('https://ronweb-api.zeabur.app/api/siteMap/article')
+      .then(({ data }) => data)
+      .then(({ data }) => data ?? [])
+      .then(array => {
+        const path = array.map(({ id }) => ({
+          url: `/blog/article/${id}`,
+          changefreq: 'daily',
+          priority: 0.9,
+          lastmod,
+        }));
+        const totalItems = array.length;
+        const itemsPerPage = 10; // 每頁筆數
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+        for (let i = 1; i <= totalPages; i++) {
+          path.push({
+            url: `/blog?page=${i}`,
+            changefreq: 'daily',
+            priority: 0.9,
+            lastmod,
+          });
+        }
+        return path;
+      })
+      .catch(error => {
+        // 處理錯誤
+        console.error(error);
+        return [];
+      }),
+    axios('https://ronweb-api.zeabur.app/api/siteMap/category')
+      .then(({ data }) => data)
+      .then(({ data }) => data ?? [])
+      .then(array =>
+        array.map(({ id }) => ({
+          url: `/blog/category/${id}`,
+          changefreq: 'weekly',
+          priority: 0.8,
+          lastmod,
+        })),
+      )
+      .catch(error => {
+        // 處理錯誤
+        console.error(error);
+        return [];
+      }),
+    axios('https://ronweb-api.zeabur.app/api/siteMap/label')
+      .then(({ data }) => data)
+      .then(({ data }) => data ?? [])
+      .then(array =>
+        array.map(({ id }) => ({
+          url: `/blog/label/${id}`,
+          changefreq: 'weekly',
+          priority: 0.8,
+          lastmod,
+        })),
+      )
+      .catch(error => {
+        // 處理錯誤
+        console.error(error);
+        return [];
+      }),
+  ]);
+  urls = [...urls, ...articles, ...categorys, ...labels];
+  // 每幾個網址分割成一個 sitemap 檔案
+  const urlsPerSitemap = 20000;
+  // 計算 sitemap 檔案數量
+  let sitemapCount = 0;
 
-// 建立 SitemapAndIndexStream
-const sms = new SitemapAndIndexStream({
-  limit: urlsPerSitemap,
-  getSitemapStream: i => {
-    const sitemapStream = new SitemapStream({ hostname: 'https://example.com' });
-    const path = `./dist/RonWeb/browser/sitemap-${i + 1}.xml`;
-    // 每次產生新的 sitemap 檔案時增加計數
-    sitemapCount = sitemapCount + 1;
-    console.warn(sitemapCount);
-    const ws = sitemapStream.pipe(createWriteStream(resolve(path)));
+  // 建立 SitemapAndIndexStream
+  const sms = new SitemapAndIndexStream({
+    limit: urlsPerSitemap,
+    getSitemapStream: i => {
+      const sitemapStream = new SitemapStream({ hostname: 'https://ronweb.zeabur.app' });
+      const path = `./dist/RonWeb/browser/sitemap-${i + 1}.xml`;
+      // 每次產生新的 sitemap 檔案時增加計數
+      sitemapCount = sitemapCount + 1;
+      const ws = sitemapStream.pipe(createWriteStream(resolve(path)));
 
-    return [new URL(path, 'https://example.com/').toString(), sitemapStream, ws];
-  },
-});
+      return [new URL(path, 'https://ronweb.zeabur.app/').toString(), sitemapStream, ws];
+    },
+  });
 
-// 添加網址資訊到 SitemapAndIndexStream
-urls.forEach(url => sms.write(url));
+  // 添加網址資訊到 SitemapAndIndexStream
+  urls.forEach(url => sms.write(url));
 
-// 結束 SitemapAndIndexStream 並建立 sitemap-index 檔案
-sms.end();
+  // 結束 SitemapAndIndexStream 並建立 sitemap-index 檔案
+  sms.end();
 
-sms.pipe(createWriteStream(resolve('./dist/RonWeb/browser/sitemap-index.xml')));
-// 等待 SitemapAndIndexStream 結束事件
-sms.on('end', () => {
-  console.log(`產生了 ${sitemapCount} 個 sitemap 檔案。`);
-});
-// fetch('https://api.example.com/endpoint')
-//   .then(response => response.json())
-//   .then(data => {
-//     // 在這裡處理接收到的回應資料
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     // 處理錯誤
-//     console.error(error);
-//   });
-// (async () => {
-//   const { data } = await axios('https://ronweb-api.zeabur.app/api/siteMap/article').catch(error => {
-//     // 處理錯誤
-//     console.error(error);
-//   })
-//   const { returnCode } = data;
-//   if (returnCode === '00') {
-//     const { data: list } = data;
-//     console.warn(JSON.stringify(list))
-//   }
-// })()
+  sms.pipe(createWriteStream(resolve('./dist/RonWeb/browser/sitemap-index.xml')));
+  // 等待 SitemapAndIndexStream 結束事件
+  sms.on('end', () => {
+    console.log(`產生了 ${sitemapCount} 個 sitemap 檔案。`);
+  });
+})();
