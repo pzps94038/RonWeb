a0b596e088e084b44a614d61e2f36526f34e68df
ron.ro <pzps94038@yahoo.com.tw>
2023-05-24 20:49:36 +0800

    fix: 新增點擊複製

--- src/app/shared/service/code-block-highlight.service.ts
+++ src/app/shared/service/code-block-highlight.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject, ChangeDetectorRef } from '@angular/core';
 import hljs from 'highlight.js';
+declare const window: any;
 @Injectable({
   providedIn: 'root',
 })
@@ -9,6 +10,16 @@ export class CodeBlockHighlightService {
       (el ?? document)
         .querySelectorAll('pre code')
         .forEach((block: Element) => this.highlightBlock(block as HTMLElement));
+      const options = {
+        loadDelay: 0,
+        copyIconClass: 'fa-solid fa-copy',
+        // // CSS class(es) used to render the done icon.
+        checkIconClass: 'fa fa-check text-success',
+        onBeforeTextCopied: function (text: any, codeElement: any) {
+          return text;
+        },
+      };
+      window.highlightJsBadge(options);
     });
   }
 
