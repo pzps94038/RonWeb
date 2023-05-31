d71b10549a7735af3c0cf73884ac89492e492df7
ron.ro <pzps94038@yahoo.com.tw>
2023-05-31 23:09:42 +0800

    fix: 調整github comment 測試案例

--- src/app/shared/component/giscus/giscus.component.spec.ts
+++ src/app/shared/component/giscus/giscus.component.spec.ts
@@ -1,6 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ThemeService } from './../../service/theme.service';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { GiscusComponent } from './giscus.component';
+import { NavigationEnd, NavigationStart } from '@angular/router';
+import { of } from 'rxjs';
+import { signal } from '@angular/core';
 
 describe('GiscusComponent', () => {
   let component: GiscusComponent;
@@ -18,4 +22,62 @@ describe('GiscusComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('測試 Init NavigationStart', fakeAsync(() => {
+    component.elementRef.nativeElement.innerHTML = '';
+    spyOnProperty(component.router, 'events', 'get').and.returnValue(
+      of(new NavigationStart(1, '')),
+    );
+    component.ngOnInit();
+    tick();
+    expect(component.elementRef.nativeElement.innerHTML).toBe('');
+  }));
+
+  it('測試 Init NavigationEnd', fakeAsync(() => {
+    component.elementRef.nativeElement.innerHTML = '';
+    spyOnProperty(component.router, 'events', 'get').and.returnValue(
+      of(new NavigationEnd(1, '', '')),
+    );
+    component.ngOnInit();
+    tick();
+    expect(component.elementRef.nativeElement.querySelector('script')).toBeTruthy();
+  }));
+
+  it('測試 Server Mode 阻擋', fakeAsync(() => {
+    component.elementRef.nativeElement.innerHTML = '';
+    spyOnProperty(component.deviceSrv, 'isClient', 'get').and.returnValue(false);
+    component.generateComment();
+    tick();
+    expect(component.elementRef.nativeElement.querySelector('script')).toBe(null);
+  }));
+});
+
+describe('GiscusComponent DarkMode', () => {
+  let component: GiscusComponent;
+  let fixture: ComponentFixture<GiscusComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [GiscusComponent],
+      providers: [
+        {
+          provide: ThemeService,
+          useFactory: () => {
+            return {
+              darkMode: signal(true),
+            };
+          },
+        },
+      ],
+    });
+    fixture = TestBed.createComponent(GiscusComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('測試 DarkComment', fakeAsync(() => {
+    component.elementRef.nativeElement.innerHTML = '';
+    component.generateComment();
+    tick();
+    expect(component.elementRef.nativeElement.querySelector('script')).toBeTruthy();
+    component.darkMode$.subscribe(isDarkMode => expect(isDarkMode).toBe(true));
+  }));
 });
