b13915dcb60bc2724b8e40133dce934b48032fcc
ron.ro <pzps94038@yahoo.com.tw>
2023-05-31 23:21:05 +0800

    fix: 調整http測試

--- src/app/shared/api/contact-us/contact-us.service.spec.ts
+++ src/app/shared/api/contact-us/contact-us.service.spec.ts
@@ -1,7 +1,11 @@
+import { ReturnCode } from 'src/app/shared/api/shared/shared.model';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ContactUsService } from './contact-us.service';
+import { BaseMessageResponse } from '../shared/shared.model';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 describe('ContactUsService', () => {
   let service: ContactUsService;
@@ -15,5 +19,20 @@ describe('ContactUsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    const returnCode = ReturnCode.Success;
+    spyOn((service as any).http as HttpClient, 'post').and.returnValue(
+      of({
+        returnCode,
+        returnMessage: '',
+      } as BaseMessageResponse),
+    );
+    service
+      .sendContactUsMail({
+        clientToken: '',
+        subject: '',
+        email: '',
+        body: '',
+      })
+      .subscribe(res => expect(res.returnCode).toBe(returnCode));
   });
 });
