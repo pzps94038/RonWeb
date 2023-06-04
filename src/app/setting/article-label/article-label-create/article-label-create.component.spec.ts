62cb8e82080368b42fb6f079f65c714c01f0f7f5
ron.ro <pzps94038@yahoo.com.tw>
2023-06-04 20:10:04 +0800

    fix: 增加測試案例

--- src/app/setting/article-label/article-label-create/article-label-create.component.spec.ts
+++ src/app/setting/article-label/article-label-create/article-label-create.component.spec.ts
@@ -1,7 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { ArticleLabelCreateComponent } from './article-label-create.component';
+import { of } from 'rxjs';
+import { ReturnCode, BaseMessageResponse } from 'src/app/shared/api/shared/shared.model';
 
 describe('ArticleLabelCreateComponent', () => {
   let component: ArticleLabelCreateComponent;
@@ -19,4 +21,25 @@ describe('ArticleLabelCreateComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('測試創建標籤', fakeAsync(() => {
+    const fake = {
+      returnCode: ReturnCode.Success,
+      returnMessage: 'msg',
+    } as BaseMessageResponse;
+    spyOn(component.articleLabelSrv, 'createArticleLabel').and.returnValue(of(fake));
+    spyOn(component.swalSrv, 'alert').and.returnValue(of({ isConfirmed: true }) as never);
+    const spy = spyOn(component.router, 'navigate');
+    component.form.controls['labelName'].setValue('labelName');
+    component.submit();
+    tick();
+    expect(spy).toHaveBeenCalled();
+  }));
+
+  it('測試創建標籤驗證沒過', fakeAsync(() => {
+    component.form.controls['labelName'].setValue('');
+    component.submit();
+    tick();
+    expect(component.form.valid).toBe(false);
+  }));
 });
