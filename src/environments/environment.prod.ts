7b665a0b9f8e797b82f7a21cb5a96ffb3032aac2
ron.ro <pzps94038@yahoo.com.tw>
2023-06-06 20:52:09 +0800

    fix: 更改為https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://ronweb-api.zeabur.app/api',
+  baseUrl: 'https://blog-api.ronwebs.com/api',
   grecaptchaToken: '6Le5DuklAAAAAPCXaSYX2vGmnTaDduUNWPZcq86u',
   shortname: 'ronweb',
 };
