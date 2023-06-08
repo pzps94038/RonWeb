263f546dd4fbf7b75be299f2426676fbc2b924e1
ron.ro <pzps94038@yahoo.com.tw>
2023-06-08 22:37:25 +0800

    fix: 新增測試案例

--- src/app/shared/component/install-app/install-app.component.spec.ts
+++ src/app/shared/component/install-app/install-app.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { InstallAppComponent } from './install-app.component';
 
@@ -18,4 +18,42 @@ describe('InstallAppComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('測試偵測pwa', fakeAsync(() => {
+    spyOnProperty(
+      window.matchMedia('(display-mode: standalone)'),
+      'matches',
+      'get',
+    ).and.returnValue(false);
+    const mockNavigator = {
+      serviceWorker: true,
+    };
+    const mockWindow = {
+      PushManager: true,
+    };
+    Object.defineProperty(window.navigator, 'serviceWorker', {
+      value: mockNavigator.serviceWorker,
+    });
+    Object.defineProperty(window, 'PushManager', {
+      value: mockWindow.PushManager,
+    });
+    window.dispatchEvent(new Event('beforeinstallprompt'));
+    tick();
+    expect(component.show()).toBe(true);
+  }));
+
+  it('測試安裝pwa', fakeAsync(async () => {
+    component.show.set(true);
+    component.deferredPrompt = {
+      prompt: () => undefined,
+      userChoice: new Promise(resolve =>
+        resolve({
+          outcome: 'accepted',
+        }),
+      ),
+    };
+    await component.install();
+    tick();
+    expect(component.show()).toBe(false);
+  }));
 });
