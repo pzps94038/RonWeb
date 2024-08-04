b18588639f7ea4c73798ad732794d156757e6c1c
ron.ro <pzps94038@yahoo.com.tw>
2024-08-05 07:29:54 +0800

    Revert "fix: API改內網"
    This reverts commit a63ec0ff80bf360d9391d657dde8f0c5c4297796.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://ronweb-api.zeabur.internal:8080/api',
+  baseUrl: 'https://ron-web-api.zeabur.app/api',
   grecaptchaToken: '6Le5DuklAAAAAPCXaSYX2vGmnTaDduUNWPZcq86u',
   shortname: 'ronweb',
 };
