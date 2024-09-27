26a37798094b0ed4fa480baade75f56b0608c719
ron.ro <pzps94038@yahoo.com.tw>
2024-09-28 00:06:55 +0800

    fix: 修正CodeType、Code  API spec

--- src/app/shared/api/admin-code-type/admin-code-type.service.spec.ts
+++ src/app/shared/api/admin-code-type/admin-code-type.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AdminCodeTypeService } from './admin-code-type.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AdminCodeTypeService', () => {
   let service: AdminCodeTypeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AdminCodeTypeService],
+    });
     service = TestBed.inject(AdminCodeTypeService);
   });
 
--- src/app/shared/api/admin-code/admin-code.service.spec.ts
+++ src/app/shared/api/admin-code/admin-code.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AdminCodeService } from './admin-code.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AdminCodeService', () => {
   let service: AdminCodeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AdminCodeService],
+    });
     service = TestBed.inject(AdminCodeService);
   });
 
