000d429aab8120e1d11ae841e5486ad747330dd5
ron.ro <pzps94038@yahoo.com.tw>
2024-09-28 03:37:31 +0800

    fix: 修正CodeCreate單元測試錯誤

--- src/app/setting/code-type/code/code-create/code-create.component.spec.ts
+++ src/app/setting/code-type/code/code-create/code-create.component.spec.ts
@@ -2,14 +2,26 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CodeCreateComponent } from './code-create.component';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { of, Observable } from 'rxjs';
 
 describe('CodeCreateComponent', () => {
   let component: CodeCreateComponent;
   let fixture: ComponentFixture<CodeCreateComponent>;
-
+  const paramMap = of({
+    get: (key: string) => 'code-type-id',
+  }) as Observable<ParamMap>;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [CodeCreateComponent, HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap,
+          } as ActivatedRoute,
+        },
+      ],
     });
     fixture = TestBed.createComponent(CodeCreateComponent);
     component = fixture.componentInstance;
